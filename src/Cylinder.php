c2d1b4eb4fd6ebcc8c77aadafb90d4248725af4a
vanvinh1234 <vanvinhbk90hoangkim@gmail.com>
2019-09-16 10:07:01 +0700

    clean code

--- src/Cylinder.php
+++ src/Cylinder.php
@@ -10,9 +10,29 @@ class Cylinder
 {
     public function getVolume($radius, $height)
     {
-        $baseArea = pi() * $radius * $radius;
-        $perimeter = 2 * pi() * $radius;
+        $baseArea = $this->getBaseArea($radius);
+        $perimeter = $this->getPerimeter($radius);
         $volume = $perimeter * $height + 2 * $baseArea;
         return $volume;
+    }
+
+    /**
+     * @param $radius
+     * @return float|int
+     */
+    public function getBaseArea($radius)
+    {
+        $baseArea = pi() * $radius * $radius;
+        return $baseArea;
+    }
+
+    /**
+     * @param $radius
+     * @return float|int
+     */
+    public function getPerimeter($radius)
+    {
+        $perimeter = 2 * pi() * $radius;
+        return $perimeter;
     }
 }
